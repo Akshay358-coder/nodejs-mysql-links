5fc7b9bf96391a47c2cade60a27285bb159c5939
Akshay358-coder <52698013+Akshay358-coder@users.noreply.github.com>
2022-08-05 21:05:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "localhost",
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "mypassword",
+  host: process.env.DATABASE_HOST || "database-2-dblinks.c8ewp26xjjep.ap-south-1.rds.amazonaws.com",
+  user: process.env.DATABASE_USER || "admin",
+  password: process.env.DATABASE_PASSWORD || "admin123",
   database: process.env.DATABASE_NAME || "dblinks",
 };
 
