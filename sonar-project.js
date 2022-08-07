f9ddff8529602513ae7f9dd9714434c08ec5c858
Akshay358-coder <52698013+Akshay358-coder@users.noreply.github.com>
2022-08-07 21:14:40 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://ec2-13-126-39-193.ap-south-1.compute.amazonaws.com:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample1',
+	    'sonar.projectKey':'Technologies',
+	   // 'sonar.login': '9d988128860aaa457ff7729bdfdf996f55cce543',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
